fb711d3c8f38be8c0f3e49f8091cc16b6c1c79db
Ville K <test@hopo.com>
2021-10-20 10:15:48 +0300

    database as env variable

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'netuser',
-  password: 'netpass',
-  database: 'library'
-});
-module.exports = connection;
-
 // const mysql = require('mysql');
-// const dotenv = require('dotenv');
-// dotenv.config();
-// const connection = mysql.createConnection(process.env.SQL_SERVER);
+// const connection = mysql.createPool({
+//   host: 'localhost',
+//   user: 'netuser',
+//   password: 'netpass',
+//   database: 'library'
+// });
 // module.exports = connection;
+
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+const connection = mysql.createConnection(process.env.database);
+module.exports = connection;
